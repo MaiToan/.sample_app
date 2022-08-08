0d93df99fa24a0ad0a80d94df839d1a1190de95a
MaiToan <maiphuctoan867gmail.com>
2022-08-08 16:09:42 +0700

    chapter 9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,15 +2,16 @@ class SessionsController < ApplicationController
   def new; end
 
 
+
   def create
     user = User.find_by email: params[:session][:email].downcase
 
     if user&.authenticate params[:session][:password]
       log_in user
-      redirect_to login_url
-      flash[:success] = t ".messages_success"
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_to user
     else
-      flash.now[:danger] = t ".messages_error"
+      flash.now[:danger] = t "invalid_email_password_combination"
       render :new
     end
   end
