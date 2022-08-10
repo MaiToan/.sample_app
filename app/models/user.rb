7e3b594017939027753eb52f532b1e9d2c8fc0ea
MaiToan <maiphuctoan867gmail.com>
2022-08-10 12:06:11 +0700

    chapter_8

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,23 @@
 class User < ApplicationRecord
   USER_ATTR = %i(name email password password_confirmation).freeze
   before_save :downcase_email
- 
+
   validates :email, presence: true, length: {minimum: Settings.user.email_min,
-   maximum: Settings.user.email_max},
-   format: {with: Settings.user.email_regex},
-   uniqueness: {case_sensitive: false}
+                                             maximum: Settings.user.email_max},
+    format: {with: Settings.user.email_regex},
+    uniqueness: {case_sensitive: false}
 
   validates :name, presence: true,
-   length: {minimum: Settings.user.name_min}
+    length: {minimum: Settings.user.name_min}
 
   validates :password, presence: true,
-   length: {minimum: Settings.user.password_min},
-   if: :password
+    length: {minimum: Settings.user.password_min},
+    if: :password
 
   has_secure_password
- private
- 
- def downcase_email
-   self.email.downcase!
- end
+  private
+
+  def downcase_email
+    email.downcase!
+  end
 end
